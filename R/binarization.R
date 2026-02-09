c856f487bba2983d57a7151596d5df1914063d4b
moi-taiga <moi.t.nicholas@gmail.com>
2026-02-09 17:20:30 +0000

    typo

--- R/binarization.R
+++ R/binarization.R
@@ -46,7 +46,7 @@ binarize_exp <- function(sce, fix_cutoff = FALSE, binarize_cutoff = 0.2, ncores 
   zerop_g <- 1 - (Matrix::rowSums(expdata != 0) / ncol(expdata))
 
   if (fix_cutoff == TRUE) {
-    exp_reduced_binary2 <- (expdata > binarize_cutoff) + 0 # Remains sparse 'dgCMatrix'
+    exp_reduced_binary <- (expdata > binarize_cutoff) + 0 # Remains sparse 'dgCMatrix'
     assays(sce)$binary <- exp_reduced_binary
     oupBinary <- data.frame(geneID = rownames(sce),
                             zerop_gene = zerop_g,
